ba6dcee4c709c2119c842173cbf5aa4b144c2f24
aya-se <kakkeno1@gmail.com>
2022-04-10 13:03:42 +0900

    delete header

--- components/Algorithms.js
+++ components/Algorithms.js
@@ -1,14 +1,14 @@
 import Words from '/public/words_dictionary.json';
 
-export const simpleAlgorithm = (greens, yellows, grays) => {
-  let wordles = Object.keys(JSON.parse(JSON.stringify(Words))).filter(
-    (value) => {
-      return value.length === 5;
-    }
-  );
-  const frequencies = [8.17, 1.49, 2.78, 4.25, 12.7, 2.23, 2.02, 6.09, 6.97, 0.15, 0.77, 4.03, 2.41, 6.75, 7.51, 1.93, 0.10, 5.99, 6.33, 9.06, 2.76, 0.98, 2.36, 0.15, 1.97, 0.07];
+// 定数
+const frequencies = [
+  8.17, 1.49, 2.78, 4.25, 12.7, 2.23, 2.02, 6.09, 6.97, 0.15, 0.77, 4.03, 2.41,
+  6.75, 7.51, 1.93, 0.1, 5.99, 6.33, 9.06, 2.76, 0.98, 2.36, 0.15, 1.97, 0.07,
+];
 
-  wordles = wordles.filter(
+// 条件に合致するワードリストを計算する共通の関数
+const wordleFilter=(wordles, greens, yellows, grays) => {
+  return wordles.filter(
     (value) => {
       for(let j = 0; j < 5; j++) {
         const idx = value[j].toUpperCase().charCodeAt(0)-65;
@@ -31,8 +31,19 @@ export const simpleAlgorithm = (greens, yellows, grays) => {
         if (yellows[idx][j]) return false;
       }
       return true;
+    });
+}
+
+// シンプルなアルゴリズム
+export const simpleAlgorithm = (greens, yellows, grays) => {
+  let wordles = Object.keys(JSON.parse(JSON.stringify(Words))).filter(
+    (value) => {
+      return value.length === 5;
     }
-  )
+  );
+  
+  wordles = wordleFilter(wordles, greens, yellows, grays);
+
   wordles.sort( (a,b) => {
     let scoreA = 0; let scoreB = 0;
     let mapA = Array(26).fill(0); let mapB = Array(26).fill(0);
@@ -46,5 +57,35 @@ export const simpleAlgorithm = (greens, yellows, grays) => {
     }
     return scoreB - scoreA;
   })
+
+  return wordles;
+};
+
+// 安全志向アルゴリズム
+export const safetyAlgorithm = (greens, yellows, grays) => {
+  let wordles = Object.keys(JSON.parse(JSON.stringify(Words))).filter(
+    (value) => {
+      return value.length === 5;
+    }
+  );
+
+  wordles = wordleFilter(wordles, greens, yellows, grays);
+
+  wordles.sort((a, b) => {
+    let scoreA = 0;
+    let scoreB = 0;
+    let mapA = Array(26).fill(0);
+    let mapB = Array(26).fill(0);
+    for (let i = 0; i < 5; i++) {
+      mapA[a[i].toUpperCase().charCodeAt(0) - 65]++;
+      mapB[b[i].toUpperCase().charCodeAt(0) - 65]++;
+    }
+    for (let i = 0; i < 26; i++) {
+      if (mapA[i] > 0) scoreA += frequencies[i] * mapA[i] ** (1 / 2);
+      if (mapB[i] > 0) scoreB += frequencies[i] * mapB[i] ** (1 / 2);
+    }
+    return scoreB - scoreA;
+  });
+
   return wordles;
 };
--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,10 @@
 import Header from './Header';
-import Footer from './Footer';
 
 export default function Layout({ children, ...props }) {
   return (
     <div>
       <Header />
       <div className="mx-3" {...props}>{children}</div>
-      <Footer />
     </div>
   );
 }
