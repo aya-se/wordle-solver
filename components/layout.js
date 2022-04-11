6a9bce8c22be951ff89139a4df657856b74eb80f
aya-se <kakkeno1@gmail.com>
2022-04-12 08:40:13 +0900

    footer入れ

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,12 @@
 import Header from './Header';
 
+
 export default function Layout({ children, ...props }) {
   return (
     <div>
       <Header />
       <div className="mx-3" {...props}>{children}</div>
+      <Footer/>
     </div>
   );
 }
